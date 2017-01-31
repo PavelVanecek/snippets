95c001933f90d086cf3466dc384c1dd8c4af3006
Pavel Vanecek <pavel.vanecek@merck.com>
2017-01-31 11:30:34 +0100

    Refresh
    - removed the test framework and questions and answers because we never used it anyway
    - added eslint-config-msd
    - refreshed examples
    - removed interval.js because it was too harsh

--- answers/basic2.js
+++
@@ -1,9 +0,0 @@
-module.exports = 2
-
-/*
-
-This question focuses on scope.
-
-function block is the only one that creates its own scope and overrides the variable.
-
-*/
--- answers/basic4.js
+++
@@ -1,9 +0,0 @@
-module.exports = undefined
-
-/*
-
-This example shows accessing properties of an object.
-
-It also shows that array is still an object.
-
-*/
